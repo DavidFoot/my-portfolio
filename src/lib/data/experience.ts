d9a3bcb421e54b6e511a38de35e9e27f9d909a77
DavidFoot <48476987+DavidFoot@users.noreply.github.com>
2024-04-05 01:41:32 +0200

    Update experience.ts

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -3,6 +3,21 @@ import { getSkills } from './skills';
 import { ContractType, type Experience } from '../types';
 
 export const items: Array<Experience> = [
+  {
+		slug: 'open-sourcer',
+		company: 'Pharmasimple',
+		description: 'Responsable IT',
+		contract: ContractType.SelfEmployed,
+		type: '',
+		location: 'Home',
+		period: { from: new Date(2016,0,1) },
+		skills: getSkills('ts', 'js'),
+		name: 'Responsable IT',
+		color: 'red',
+		links: [],
+		logo: Assets.Unknown,
+		shortDescription: 'Gestion du département IT'
+	},
 ];
 
 export const title = 'Expériences';
