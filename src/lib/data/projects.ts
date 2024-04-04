633f54081fb794b887f419d2aac60a4dd13b60d0
DavidFoot <48476987+DavidFoot@users.noreply.github.com>
2024-04-05 01:25:14 +0200

    Update projects.ts

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -17,7 +17,13 @@ export const items: Array<Project> = [
 			from: new Date()
 		},
 		skills: getSkills('csharp'),
-		type: 'Jeu 2D basé sur FlappyBird'
+		type: 'Jeu 2D basé sur FlappyBird',
+		screenshots: [
+			{
+				label: 'Capture',
+				src: 'https://i.postimg.cc/1XXHWZFG/Flappy.png'
+			}
+		]
 	},
 	{
 		slug: 'slick-portfolio-csharp',
@@ -36,28 +42,8 @@ export const items: Array<Project> = [
 		type: 'Jeu de type RPG 3D ',
 		screenshots: [
 			{
-				label: 'screen 1',
+				label: 'Capture',
 				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
 			}
 		]
 	}
